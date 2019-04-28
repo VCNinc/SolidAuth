051476da3eb5273300c758e6847630ecd8fd148e
xz33 <xz33@illinois.edu>
2019-04-28 16:00:06 -0500

    insecure-unfair demo

---
+++ demos/lottery/insecure-unfair/insecure-unfair/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Redirect } from 'react-router-dom';
+
+
+class App extends Component {
+  
+  render() {
+    return (
+      <>
+      <h1>Home Page</h1>
+      </>
+    );
+  }
+    
+}
+
+export default App;
